6bb1212fe0cc91d9a907f5ab28196b837f1168a8
Guilherme <guilhermealvesportela37@gmail.com>
2024-10-16 17:53:47 +0000

    16102024

--- Lubuntu Config/Config.sh
+++ Lubuntu Config/Config.sh
@@ -2,9 +2,6 @@
 
 sudo apt update
 
-sudo apt install git -y
-
-sudo apt install python3 python3-pip python3-virtualenv pipx -y
 
 sudo apt install flatpak -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
@@ -20,8 +17,4 @@ sudo usermod -aG docker ${USER}
 su - ${USER}
 groups
 sudo usermod -aG docker guilherme
-
-python3 -m venv $VENV_PATH
-$VENV_PATH/bin/pip install -U pip setuptools
-$VENV_PATH/bin/pip install poetry
 
--- Lubuntu Config/Install.sh
+++ Lubuntu Config/Install.sh
@@ -6,3 +6,8 @@ flatpak install flathub com.github.libresprite.LibreSprite -y
 flatpak install flathub org.godotengine.Godot -y
 flatpak install flathub com.github.sdv43.whaler -y
 flatpak install flathub com.anydesk.Anydesk -y
+
+sudo apt install python3 python3-pip python3-virtualenv pipx git tilix -y
+python3 -m venv $VENV_PATH
+$VENV_PATH/bin/pip install -U pip setuptools
+$VENV_PATH/bin/pip install poetry
