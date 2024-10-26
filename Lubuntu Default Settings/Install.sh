ca3ebd69ad80864177df91d033a5498541708dcd
Guilherme <guilhermealvesportela37@gmail.com>
2024-10-25 22:02:57 -0300

    Update Install.sh

--- Lubuntu Default Settings/Install.sh
+++ Lubuntu Default Settings/Install.sh
@@ -6,6 +6,8 @@ flatpak install flathub com.github.libresprite.LibreSprite -y
 flatpak install flathub org.godotengine.Godot -y
 flatpak install flathub com.github.sdv43.whaler -y
 flatpak install flathub com.anydesk.Anydesk -y
+flatpak install flathub org.kde.kdenlive -y
+flatpak install flathub com.google.Chrome -y
 
 sudo apt install python3 python3-pip python3-virtualenv pipx git tilix -y
 python3 -m venv $VENV_PATH
